6fe604e1bead478ed8d4e1cb59b60f8e2dc23679
Val <sudaryk@gmail.com>
2018-01-17 17:51:53 +0800

    change article color

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -4,6 +4,8 @@ function SelectSize(Element)
 	var article = $(Element).parents('.dl-horizontal').children('.article').html();
 	var name = $('#panel-title-' + article).html();
 
+	var changeArticle = $(Element).parents('.dl-horizontal').children('.article').css('color', 'red');
+
 	console.log('Size is: ' + size + ', name is: ' + name);
 
     return false;
